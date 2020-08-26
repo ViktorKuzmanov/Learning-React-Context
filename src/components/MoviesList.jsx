c544d437a29f0fc7f6a22fdbff2a6fa1848d3d9c
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-26 16:18:54 +0200

    add MovieContext

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 import Movie from "./Movie";
 
 const MovieList = () => {
-  const [movies, setMovies] = useState([
-    { name: "Movie1", price: "$11", id: 111 },
-    { name: "Movie2", price: "$22", id: 222 },
-    { name: "Movie3", price: "$33", id: 333 },
-  ]);
   return (
     <div>
       {movies.map((movie) => (
