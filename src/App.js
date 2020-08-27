c6a14c363862fa334a09d4fbc887442c2b7a1e38
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-27 17:21:36 +0200

    show nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MoviesList from "./components/MoviesList";
 import { MovieProvider } from "./context/MovieContext";
+import Nav from "./components/Nav";
 
 function App(props) {
   return (
     <MovieProvider>
+      <Nav />
       This is app component
       <MoviesList />
     </MovieProvider>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+const greenBackground = { backgroundColor: "green" };
+
 const Nav = () => {
   return (
-    <div>
-      <h3>Viktor</h3>
+    <div style={greenBackground}>
+      <h3>Movies app</h3>
       <p>List of movies:</p>
     </div>
   );
 };
+
+export default Nav;
