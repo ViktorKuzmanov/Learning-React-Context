ffeb35a0b8baa60faa48883304497914fca04ae5
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-28 21:06:55 +0200

    show number of movies from context

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
 
 const greenBackground = { backgroundColor: "green" };
 
 const Nav = () => {
+  const [movies, setMovies] = useContext(MovieContext);
   return (
     <div style={greenBackground}>
       <h3>Movies app</h3>
-      <p>List of movies:</p>
+      <p>Number of movies: {movies.length}</p>
     </div>
   );
 };
